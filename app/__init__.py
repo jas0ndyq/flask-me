807fbb46f1ff0be6ae85a5ca4a75e755327d6410
jasondyq <jasondyq@foxmail.com>
2015-05-25 14:45:10 +0800

    complete migrate db

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.bcrypt import Bcrypt
-from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -15,8 +14,7 @@ db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
 
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+
 
 from app import views, forms, models
 
